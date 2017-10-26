5c5a98adb7d95e59c7db6209cf432e8b450bd79a
burlamix <burlamix@gmail.com>
2017-10-26 15:09:37 +0200

    stati

--- reverse_up_down/main.py
+++ reverse_up_down/main.py
@@ -5,12 +5,14 @@ from parser import *
 from parser_class import *
 from E_M_utils import *
 
-FILE1 = "inex05.train.elastic.tree"
-FILE2 = "inex05.train.elastic.tree"
-#FILE1 = "test_2.tree"
-#FILE2 = "test_2.tree"
+#FILE1 = "inex05.train.elastic.tree"
+#FILE2 = "inex05.train.elastic.tree"
+FILE1 = "test_2.tree"
+FILE2 = "test_2.tree"
+#FILE1 = "train_100.tree"
+#FILE2 = "test_100.tree"
 
-epoche = 30
+epoche = 3
 
 data_set = dataset_parser_class(FILE1)
 data_test = dataset_parser(FILE2)
