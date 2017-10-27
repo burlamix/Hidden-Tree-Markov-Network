b3c9589e79d77be8d95033d507a2c516c035e95c
burlamix <burlamix@gmail.com>
2017-10-27 06:37:11 +0200

    gpu

--- reverse_up_down/E_M_utils.py
+++ reverse_up_down/E_M_utils.py
@@ -11,12 +11,12 @@ N_HIDDEN_STATES = 8
 N_SYMBOLS = 367
 MAX_CHILD = 32
 CLASSI = 2
-GPU = False
+GPU = True
 
 
 
 config = tf.ConfigProto(log_device_placement=GPU)
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
+#config.gpu_options.per_process_gpu_memory_fraction = 0.5
 #config.gpu_options.allow_growth = True
 
 def modello(data_set,epoche):
