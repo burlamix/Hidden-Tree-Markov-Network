49a6fb261e9a248dc8b439efa2f1338c26225364
burlamix <burlamix@gmail.com>
2017-11-06 00:54:06 +0100

    .

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ epoche = 1
 
 hidden_state = 10
 
-batch_size = 9
+batch_size = 32
 
 #TRANIG SET
-#traning_set = "inex05.train.elastic.tree"
-traning_set = "data/train_100.tree"
+traning_set = "inex05.train.elastic.tree"
 #traning_set = "data/train_100.tree"
+#traning_set = "data/train_4.tree"
 
 #TEST SET
 #test_set = "inex05.test.elastic.tree"
-test_set = "data/test_100.tree"
 #test_set = "data/test_100.tree"
+test_set = "data/test_4.tree"
 
 data_train = dataset_parser(traning_set)
 
@@ -35,9 +35,9 @@ modello = HTM(m)
 htm , lamda = training(modello,hidden_state,m,lerning_rate,epoche,batch_size,data_train)
 
 
-print("test...")
+#print("test...")
 
-result 		= test(htm,lamda,data_test,m,hidden_state)
+#result 		= test(htm,lamda,data_test,m,hidden_state)
 
 
 print("\n\n                         ",result)
